1134d9863c358a37e45cfb0cb7c6e2ff9230d6a7
ankahngg <ankhanghp2004@gmail.com>
2024-06-03 23:39:28 +0700

    fix title

--- desktop/src/com/badlogic/drop/DesktopLauncher.java
+++ desktop/src/com/badlogic/drop/DesktopLauncher.java
@@ -2,13 +2,15 @@ package com.badlogic.drop;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.drop.CuocChienSinhTon;
 
 public class DesktopLauncher {
    public static void main (String[] arg) {
       Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-      config.setWindowIcon();
-      config.setTitle("HungDz");
+      //Texture icon = new Texture("")
+      config.setWindowIcon("icon2.jpg");
+      config.setTitle("Cuộc chiến sinh tồn");
       config.setWindowedMode(CuocChienSinhTon.V_WIDTH*2,CuocChienSinhTon.V_HEIGHT*2 );
       config.useVsync(true);
       config.setForegroundFPS(60);
